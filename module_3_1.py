cd040a164905cb08b1de51665140ad39d1e8990f
Akella348 <akella348@gmail.com>
2024-07-07 17:50:01 +0400

    доделал

--- module_3_1.py
+++ module_3_1.py
@@ -1,6 +1,23 @@
 calls = 0
 def count_calls():
-    global calls
-    calls +=1
-def string_info():
-def
+    global calls  # вызов глобальной переменной
+    calls += 1  # добавляем единицу
+def string_info(string):
+    count_calls()  # вызов функции подсчета вызовов
+    string_par = (len(string), string.upper(), string.lower())  # вводим переменную, в которую кладем длину, верхний
+# и нижний регистр
+    return string_par  # возвращаем переменную
+def is_contains(string, list_to_search):  # функция по проверке элементов содержащихся в списке
+    count_calls()  # вызов функции подсчета вызовов
+    contains_marker = False  # базовый маркер
+    for i in list_to_search:  # перебор элементов и проверка их по условию
+        if string.lower() == i.lower():  # если string в нижнем регистре равен i
+            contains_marker = True  # тогда переключаем маркер и прерываем цикл
+            break
+    return contains_marker  # возвращаем маркер
+print(string_info('Armageddon'))
+print(string_info('Tequila'))
+print(calls)
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
