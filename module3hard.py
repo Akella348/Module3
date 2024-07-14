ce7edf0d0640f62486a5bf6433fba3f33ec4fc8d
Akella348 <akella348@gmail.com>
2024-07-14 22:18:02 +0400

    комментарии

--- module3hard.py
+++ module3hard.py
@@ -9,22 +9,22 @@ def calculate_structure_sum(data_structure):
     total_sum = 0
     total_len = 0
     def structure_checker(item):
-        nonlocal total_sum, total_len
-        if isinstance(item, (list, tuple, set)):
-            for object in item:
-                structure_checker(object)
-        elif isinstance(item, dict):
-            for key, value in item.items():
-                structure_checker(key)
-                structure_checker(value)
-        elif isinstance(item, int):
-            total_sum += item
-        elif isinstance(item, float):
-            total_sum += int(item)
-        elif isinstance(item, str):
-            total_len += len(item)
-    structure_checker(data_structure)
-    return total_sum + total_len
+        nonlocal total_sum, total_len  # обращаемся к введенным в головной функции переменным
+        if isinstance(item, (list, tuple, set)):  # вводим проверку класса на список кортеж или множество
+            for object in item:  # и каждый объект из взятого элемента
+                structure_checker(object)  # рекурсивно обращаемся по функции
+        elif isinstance(item, dict):  # вводим проверку класса на словарь
+            for key, value in item.items():  # и каждый объект из взятого элемента
+                structure_checker(key)  # рекурсивно обращаемся по функции для ключа
+                structure_checker(value)  # рекурсивно обращаемся по функции для значения
+        elif isinstance(item, int):  # вводим проверку класса на целое число
+            total_sum += item  # добавляем его в переменную
+        elif isinstance(item, float):  # вводим проверку класса на число с плавающей точкой
+            total_sum += int(item)  # добавляем его в переменную
+        elif isinstance(item, str):  # вводим проверку класса на строку
+            total_len += len(item)  # добавляем его в переменную
+    structure_checker(data_structure)  # обращаемся к функции с вводом исходника
+    return total_sum + total_len  # суммируем
 
 result = calculate_structure_sum(data_structure)
 print(result)
